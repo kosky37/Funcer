51da8a97a99076793042050e19e0456adceff340
Piotr Nowakowski <piotr121993@gmail.com>
2023-10-01 15:24:34 +0200

    Allow Create ValueResult with boolean

--- Funcer.Tests/Create/ValueResultTests.Create.cs
+++ Funcer.Tests/Create/ValueResultTests.Create.cs
@@ -7,6 +7,14 @@ public class ValueResultTests_Create
     [Fact]
     public void Should_Create_Success()
     {
+        var result = Result.Create(true, Values.Alpha1, Values.TestError);
+
+        result.ShouldBeSuccess(Values.Alpha1);
+    }
+    
+    [Fact]
+    public void Should_Create_Success_From_Function()
+    {
         var result = Result.Create(Functions.Returns.True, Values.Alpha1, Values.TestError);
 
         result.ShouldBeSuccess(Values.Alpha1);
@@ -14,6 +22,14 @@ public class ValueResultTests_Create
     
     [Fact]
     public void Should_Create_Failure()
+    {
+        var result = Result.Create(false, Values.Alpha1, Values.TestError);
+
+        result.ShouldBeFailure();
+    }
+    
+    [Fact]
+    public void Should_Create_Failure_From_Function()
     {
         var result = Result.Create(Functions.Returns.False, Values.Alpha1, Values.TestError);
 
